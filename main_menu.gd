46e62a6f97702e786c58737091b5f9df49d27f6a
Ethann Yakabuski <ethann.yakabuski@carleton.ca>
2024-11-15 22:40:43 -0500

    implements unlock character and updatees saved cloud data properly

--- main_menu.gd
+++ main_menu.gd
@@ -26,6 +26,7 @@ func _ready():
 				#$DebugLabel.text = $DebugLabel.text + " snap not found" 
 				print("snap shot not found")
 				var saveData = {"coins": 0, "playerUnlocks": [true,false]}
+				updateCoins(0)
 				var jsonSaveData = JSON.stringify(saveData)
 				SnapshotsClient.save_game("playerData", "player data for Animal Dash", jsonSaveData.to_utf8_buffer())
 			#$DebugLabel.text = $DebugLabel.text + "here1"
@@ -51,8 +52,7 @@ func _ready():
 	SnapshotsClient.game_saved.connect(
 		func(is_saved: bool, save_data_name: String, save_data_description: String):
 			if is_saved:
-				pass 
-				#$DebugLabel.text = $DebugLabel.text + " game saved"
+				updateCharacter()
 			else: 
 				pass
 				#$DebugLabel.text = $DebugLabel.text + " unable to save game"
@@ -99,6 +99,7 @@ func _on_button_pressed():
 	$CoinLabelSprite.visible = false
 	$CoinsLabel.visible = false
 	$LockIcon.visible = false
+	$UnlockButton.visible = false
 	#remove_child($CharacterImage)
 	#remove_child($StartGame)
 	#remove_child($LeftButton)
@@ -113,6 +114,9 @@ func _on_button_pressed():
 	
 func _on_coins_collected(amount): 
 	redoMainMenu()
+	$CoinsLabel.text = amount
+	
+func updateCoins(amount): 
 	$CoinsLabel.text = amount
 	
 func _on_game_finished(): 
@@ -149,8 +153,12 @@ func checkCharacterUnlock(currentCharacter):
 		#$DebugLabel.text = $DebugLabel.text + str(isCharacterUnlocked)
 	if isCharacterUnlocked == false: 
 		$LockIcon.visible = true
+		$UnlockButton.visible = true
+		$StartGame.visible = false
 	else: 
 		$LockIcon.visible = false
+		$UnlockButton.visible = false
+		$StartGame.visible = true
 	$CharacterImage.texture = load(characters[currentCharacter])
 	
 func _on_left_button_pressed():
@@ -200,3 +208,20 @@ func _on_sound_toggle_pressed() -> void:
 	
 func _on_menu_music_finished() -> void:
 	$MenuMusic.play()
+
+
+func _on_unlock_button_pressed() -> void:
+	print("unlocking a new character")
+	$DebugLabel.text = $DebugLabel.text + "unl new char"
+	if int(savedData["coins"]) >= 300: 
+		$DebugLabel.text = $DebugLabel.text + " unlocked"
+		var newCoins = int(savedData["coins"]) - 300
+		var playerUnlocks = savedData["playerUnlocks"]
+		playerUnlocks[currentCharacter] = true
+		var saveData = {"coins": newCoins, "playerUnlocks": playerUnlocks}
+		var jsonSaveData = JSON.stringify(saveData)
+		savedData = saveData
+		updateCoins(newCoins)
+		SnapshotsClient.save_game("playerData", "player data for Animal Dash", jsonSaveData.to_utf8_buffer())
+	else: 
+		$DebugLabel.text = $DebugLabel.text + " ins. funds"
