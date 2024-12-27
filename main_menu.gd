c20437d643ec6041183ef39ef11d8fbeef43d224
Ethann Yakabuski <ethann.yakabuski@carleton.ca>
2024-12-27 08:50:11 -0500

    remove debug logging

--- main_menu.gd
+++ main_menu.gd
@@ -103,39 +103,40 @@ func _ready():
 	var onInitializationCompleteListener = OnInitializationCompleteListener.new()
 	onInitializationCompleteListener.on_initialization_complete = onAdInitializationComplete
 	var request_configuration = RequestConfiguration.new()
-	$DebugLabel.text = $DebugLabel.text + "calling init"
+	#$DebugLabel.text = $DebugLabel.text + "calling init"
 	MobileAds.initialize(onInitializationCompleteListener)
-	$DebugLabel.text = $DebugLabel.text + " post init"
+	#$DebugLabel.text = $DebugLabel.text + " post init"
 	#"F03226F1DD8EFC77"
 	#,"2077EF9A63D2B398840261C8221A0C9B"
 	if MobileAds: 
-		request_configuration.test_device_ids = ["523a1b0eb5b6be122cd04bedf8035291","2077EF9A63D2B398840261C8221A0C9B"]
+		#request_configuration.test_device_ids = ["523a1b0eb5b6be122cd04bedf8035291","2077EF9A63D2B398840261C8221A0C9B"]
 		MobileAds.set_request_configuration(request_configuration)
-		_create_ad_view()
-		check_initialization_status()
+		#_create_ad_view()
+		#check_initialization_status()
 	
 func check_initialization_status():
-	$DebugLabel.text = $DebugLabel.text + "check init"
+	#$DebugLabel.text = $DebugLabel.text + "check init"
 	var status = MobileAds.get_initialization_status()
 	if status: 
 		for adapter_name in status.adapter_status_map.keys():
 			var adapter_status = status.adapter_status_map[adapter_name]
-			$DebugLabel.text = $DebugLabel.text + "Adapter: " + adapter_name + "State: " + adapter_status.state + "Description: " + adapter_status.description
+			#$DebugLabel.text = $DebugLabel.text + "Adapter: " + adapter_name + "State: " + adapter_status.state + "Description: " + adapter_status.description
 		
 func onAdInitializationComplete(status : InitializationStatus): 
 	print("banner ad initialization complete")
-	$DebugLabel.text = $DebugLabel.text + " inside init"
+	#$DebugLabel.text = $DebugLabel.text + " inside init"
 	_create_ad_view()
 
 func _create_ad_view() -> void:
-	$DebugLabel.text = $DebugLabel.text + "inside create"
+	#$DebugLabel.text = $DebugLabel.text + "inside create"
 	#free memory
 	if _ad_view:
 		destroy_ad_view()
 
 	var adListener = AdListener.new()
-	adListener.on_ad_failed_to_load = func(load_ad_error : LoadAdError): 
-		$DebugLabel.text = $DebugLabel.text + load_ad_error.message
+	adListener.on_ad_failed_to_load = func(load_ad_error : LoadAdError):
+		pass 
+		#$DebugLabel.text = $DebugLabel.text + load_ad_error.message
 		
 	#$DebugLabel.text = $DebugLabel.text + " inside create"
 	var unit_id = "ca-app-pub-3940256099942544/6300978111"
@@ -144,7 +145,7 @@ func _create_ad_view() -> void:
 	var ad_request = AdRequest.new()
 	_ad_view.load_ad(ad_request)
 	_ad_view.show()
-	$DebugLabel.text = $DebugLabel.text + " aft load"
+	#$DebugLabel.text = $DebugLabel.text + " aft load"
 	
 func destroy_ad_view(): 
 	_ad_view.destroy()
